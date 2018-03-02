614bcfe0ca59efcb04eac6a89c84da6563be5219
Jose <Perez@Joses-MacBook-Air.local>
2018-03-01 21:10:13 -0800

    ports are listening and html connected

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+
+
+// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+
+
+var friendsArray = [
+  {
+    "name":"Arnold Schwarzenegger",
+    "photo":"http://3.bp.blogspot.com/-VrKa9CkxnwM/T4Z1g8EXGeI/AAAAAAAAB08/ovjuNiBaFnY/s1600/Arnold-Schwarzenegger-06.jpeg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Arnold Schwarzenegger",
+    "photo":"http://3.bp.blogspot.com/-VrKa9CkxnwM/T4Z1g8EXGeI/AAAAAAAAB08/ovjuNiBaFnY/s1600/Arnold-Schwarzenegger-06.jpeg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Bruce lee",
+    "photo":"https://www.hublot.com/images/News/brucelee_launch_20.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Taylor Swift",
+    "photo":"https://c1.staticflickr.com/9/8701/17019827941_a1d6581d4e_b.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+
+module.exports = friendsArray
